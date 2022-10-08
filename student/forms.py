78072d430af99243e9fb66719e85704f68fd0960
Ratana10 <sanratana18@gmail.com>
2022-10-09 03:54:54 +0700

    second commit

--- student/forms.py
+++ student/forms.py
@@ -1,11 +1,12 @@
+from cProfile import label
 from django import forms
 
 class StudForm(forms.Form):
-    s_name = forms.CharField(max_length = 30)
-    s_class = forms.CharField(max_length = 30)
-    s_address = forms.CharField(max_length = 30)
-    s_school = forms.CharField(max_length = 30)
-    s_email = forms.CharField(max_length = 30)
+    s_name = forms.CharField(max_length = 30, label="Student Name ")
+    s_class = forms.CharField(max_length = 30, label="Student Class ")
+    s_address = forms.CharField(max_length = 30, label="Student Address")
+    s_school = forms.CharField(max_length = 30, label="School Name")
+    s_email = forms.CharField(max_length = 30, label= "Email")
     
     
 class SForm(forms.Form):
